8b70465f6efcecaf1e82603a87f82db470ac024a
Shashwat Ghimire <shashwatghimire28@gmail.com>
2026-02-25 15:30:40 +0545

    Update

--- reactprj/src/components/Testing.jsx
+++ reactprj/src/components/Testing.jsx
@@ -17,9 +17,7 @@ function Testing() {
     prevValue.current = inputVal;
   }, [inputVal]);
 
-  function focusInput() {
-    inputRef.current && inputRef.current.focus();
-  }
+ 
 
   return (
     <>
@@ -33,11 +31,11 @@ function Testing() {
         value={inputVal}
         onChange={(x) => setInputVal(x.target.value)}
       />
-      <button onClick={focusInput} style={{marginLeft: '10px'}}>Focus Input</button>
+      
       <h2>Current value: {inputVal}</h2>
       <h3>Previous value: {prevValue.current}</h3>
       <h3>Render count: {renderCount.current}</h3>
-      <p style={{fontStyle:'italic', color:'gray'}}>Try changing the input and clicking the focus button!</p>
+    
     </>
   );
 }
